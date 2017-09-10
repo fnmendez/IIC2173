e8dc7d3718b585e6ba7c86f98d38e334de279a58
Franco Mendez <fnmendez@uc.cl>
2017-09-10 17:50:53 -0300

    Initialize repository

---
+++ app.js
@@ -0,0 +1,42 @@
+var
+  port = 8080,
+  http = require('http'),
+  fs = require('fs');
+var
+  postLog = function(entry) {
+    fs.appendFileSync('./requests.log', new Date().toUTCString() + ' - Request from: ' + entry + '\n');
+  };
+var
+  getLog = function(n) {
+    var ret = '';
+    var file = fs.readFileSync('./requests.log', 'utf8').split('\n');
+    for (i = file.length - 1; i >= file.length - (n + 1) && i >= 0; i--) {
+        ret += file[i] + '\n';
+    }
+    return ret;
+  };
+var
+  getUserInfo = function(req) {
+    var ret = "";
+    if (req.headers['x-forwarded-for']) {
+        ret += req.headers['x-forwarded-for'].split(',')[0];
+    } else if (req.connection && req.connection.remoteAddress) {
+        ret += req.connection.remoteAddress;
+    } else {
+        ret += req.ip;
+    }
+    return ret;
+  };
+var
+  server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+      postLog(getUserInfo(req));
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Information from last 15 requests:\n' + getLog(15));
+    res.end();
+  });
+
+server.listen(port);
+
+console.log('Server running at port: ' + port);
